58f42c19fb6ad8d88cabfc9eb36593344b8dc2af
Gursharndeep Singh <gursharn9100@gmail.com>
2025-10-13 21:48:32 +0530

    Time: 3 ms (15.34%), Space: 16 MB (6.34%) - LeetHub

---
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    char find(char c ,unordered_map<char,char> &parent){
+        if(parent[c]!=c) parent[c]=find(parent[c],parent);
+        return parent[c];
+    }
+    void uni(string s,unordered_map<char,char> &parent){
+        char x=find(s[0],parent);
+        char y=find(s[3],parent);
+        if(x!=y) parent[x]=y;
+    }
+    bool equationsPossible(vector<string>& equations) {
+        unordered_map<char,char> parent;
+        for(char c='a';c<='z';c++) parent[c]=c;
+        unordered_set<string> tochk;
+        for( string &s: equations){
+            if(s[1]=='!') tochk.insert(s);
+            else uni(s,parent);
+        }
+        for (auto &[v1,v2]:parent) parent[v1]=find(v1,parent);
+        for(auto &s: tochk){
+        char x=find(s[0],parent);
+        char y=find(s[3],parent);
+        if(x==y) return false;
+        }
+        return true;
+    }
+};
