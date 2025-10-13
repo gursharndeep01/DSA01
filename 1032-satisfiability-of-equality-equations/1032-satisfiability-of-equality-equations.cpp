abda705714e3c7a5a11ab60d7f97c608e61b09d2
Gursharndeep Singh <gursharn9100@gmail.com>
2025-10-13 21:52:38 +0530

    Time: 2 ms (18.12%), Space: 16 MB (7.37%) - LeetHub

--- 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
+++ 1032-satisfiability-of-equality-equations/1032-satisfiability-of-equality-equations.cpp
@@ -19,8 +19,8 @@ public:
         }
         for (auto &[v1,v2]:parent) parent[v1]=find(v1,parent);
         for(auto &s: tochk){
-        char x=find(s[0],parent);
-        char y=find(s[3],parent);
+        char x=parent[s[0]];
+        char y=parent[s[3]];
         if(x==y) return false;
         }
         return true;
