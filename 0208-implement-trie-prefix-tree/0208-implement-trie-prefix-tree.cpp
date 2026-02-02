63b6dae7b196d7e8583aedfe0d5751ee53fec837
Gursharndeep Singh <gursharn9100@gmail.com>
2026-02-02 11:46:10 +0530

    Time: 18 ms (72.38%), Space: 50.6 MB (33.49%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,65 +1,46 @@
-class trienode{
-public:
-    char data;
-    trienode* children[26];
-    bool isend;
-    trienode(char ch){
-        data=ch;
-        for(int i=0;i<26;i++) children[i]= NULL;
-        isend=false;
-    }
-};
 class Trie {
-public:    
-    trienode* root;
+private:
+    struct Node {
+        bool isEnd;
+        Node* child[26];
+        Node() {
+            isEnd = false;
+            for (int i = 0; i < 26; i++) child[i] = nullptr;
+        }
+    };
+    
+    Node* root;
+public:
     Trie() {
-        root =new trienode('0');
-    }
-    void inserthelp(trienode* root,string word){
-        if(word.size()==0) {
-            root->isend =true;
-            return;
-        }
-        int i= word[0]-'a';
-        trienode* child;
-        if(root->children[i]!= NULL){
-            child= root->children[i];
-        }
-        else{
-            child =new trienode(word[0]);
-            root->children[i]=child;
-        }
-        inserthelp(child,word.substr(1));
+        root =new Node();
     }
     void insert(string word) {
-        inserthelp(root,word);
-    }
-    bool searchelp(trienode* root,string word){
-        if(word.size()==0) return root->isend;
-        int i= word[0]-'a';
-        trienode* child;
-        if(root->children[i]!= NULL){
-            child= root->children[i];
+        Node* cur = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!cur->child[idx])
+                cur->child[idx] = new Node();
+            cur = cur->child[idx];
         }
-        else return false;
-        return searchelp(child,word.substr(1));
+        cur->isEnd = true;
     }
     bool search(string word) {
-        return searchelp(root,word);
-    }
-    
-    bool start(trienode* root,string word){
-        if(word.size()==0) return true;
-        int i= word[0]-'a';
-        trienode* child;
-        if(root->children[i]!= NULL){
-            child= root->children[i];
+        Node* cur = root;
+        for (char c : word) {
+            int idx = c - 'a';
+            if (!cur->child[idx]) return false;
+            cur = cur->child[idx];
         }
-        else return false;
-        return start(child,word.substr(1));
+        return cur->isEnd;
     }
     bool startsWith(string prefix) {
-        return start(root,prefix);
+        Node* cur = root;
+        for (char c : prefix) {
+            int idx = c - 'a';
+            if (!cur->child[idx]) return false;
+            cur = cur->child[idx];
+        }
+        return true;
     }
 };
 
