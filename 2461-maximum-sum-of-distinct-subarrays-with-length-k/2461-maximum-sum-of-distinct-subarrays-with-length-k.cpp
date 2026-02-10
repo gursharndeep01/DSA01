6a2fac963ac262e8e49183f5f99dd085dc1e0e6a
Gursharndeep Singh <gursharn9100@gmail.com>
2026-02-10 20:53:43 +0530

    Time: 106 ms (49.42%), Space: 95.2 MB (93.96%) - LeetHub

--- 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2461-maximum-sum-of-distinct-subarrays-with-length-k/2461-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -2,25 +2,22 @@ class Solution {
 public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         long long no=0,sum=0;
-        queue<int> q;
         unordered_map<int,int> mpp;
+        int j=0;
         for(int i=0;i<nums.size();i++){
             sum+=nums[i];
-            q.push(nums[i]);
-            if(mpp.count(nums[i])){
-                i=mpp[nums[i]];
-                mpp.clear();
-                while(!q.empty()) q.pop (); 
-                sum=0;
-                continue;
+            mpp[nums[i]]++;
+
+            while(mpp[nums[i]]>1 || (i-j+1) >k){
+                mpp[nums[j]]--;
+                sum-=nums[j];
+                if(mpp[nums[j]]==0) mpp.erase(nums[j]);
+                j++;
             }
-            mpp[nums[i]]=i;
-            if(q.size()>k){
-                int ii=q.front();q.pop();
-                mpp.erase(ii);
-                sum-=ii;
+
+            if((i-j+1)==k){
+                no=max(no,sum);
             }
-            if(sum>no && q.size()==k) no=sum;
         }
         return no;
     }
