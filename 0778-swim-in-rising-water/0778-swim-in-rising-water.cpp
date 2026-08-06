2477614188039338ad1305d5e9ffe09c6de6a82b
Gursharndeep Singh <gursharn9100@gmail.com>
2026-08-06 21:14:41 +0530

    Time: 14 ms (30.49%), Space: 15.1 MB (29.92%) - LeetHub

---
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int swimInWater(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<vector<bool>> visited(n, vector<bool>(n, false));
+        
+        // min-heap: {time, i, j}
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        
+        pq.push({grid[0][0], 0, 0});
+        visited[0][0] = true;
+        int maxTime = 0;
+        
+        int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
+        
+        while (!pq.empty()) {
+            auto top = pq.top();
+            pq.pop();
+            int wt = top[0], i = top[1], j = top[2];
+            
+            maxTime = max(maxTime, wt);
+            
+            if (i == n-1 && j == n-1) return maxTime;
+            
+            for (auto& d : dirs) {
+                int ni = i + d[0];
+                int nj = j + d[1];
+                if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
+                    visited[ni][nj] = true;
+                    pq.push({grid[ni][nj], ni, nj});
+                }
+            }
+        }
+        
+        return maxTime;
+    }
+};
