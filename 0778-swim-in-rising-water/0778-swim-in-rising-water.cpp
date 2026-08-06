aff5659ea842bddc247263065eae0b0aca8f5641
Gursharndeep Singh <gursharn9100@gmail.com>
2026-08-06 21:41:41 +0530

    Time: 8 ms (49.57%), Space: 14.4 MB (38.56%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    int swimInWater(vector<vector<int>>& grid) {
-        int n = grid.size();
+    int n;
+    bool canReach(vector<vector<int>>& grid, int t) {
+        if (grid[0][0] > t) return false;
         vector<vector<bool>> visited(n, vector<bool>(n, false));
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
-        pq.push({grid[0][0], 0, 0});
+        queue<pair<int,int>> q;
+        q.push({0, 0});
         visited[0][0] = true;
-        int maxTime = 0;
         int dirs[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};
-        while (!pq.empty()) {
-            auto top = pq.top();
-            pq.pop();
-            int wt = top[0], i = top[1], j = top[2];
-            maxTime = max(maxTime, wt);
-            if (i == n-1 && j == n-1) return maxTime;
+        while (!q.empty()) {
+            auto [i, j] = q.front();
+            q.pop();
+            if (i == n-1 && j == n-1) return true;
             for (auto& d : dirs) {
                 int ni = i + d[0];
                 int nj = j + d[1];
-                if (ni >= 0 && ni < n && nj >= 0 && nj < n && !visited[ni][nj]) {
+                if (ni >= 0 && ni < n && nj >= 0 && nj < n 
+                    && !visited[ni][nj] && grid[ni][nj] <=t) {
                     visited[ni][nj] = true;
-                    pq.push({grid[ni][nj], ni, nj});
+                    q.push({ni, nj});
                 }
             }
         }
-        return maxTime;
+        return visited[n-1][n-1];
+    }
+    int swimInWater(vector<vector<int>>& grid) {
+        n = grid.size();
+        int low = 0, right = n * n - 1;
+        while (low < right) {
+            int t = low + (right - low) / 2;
+            if (canReach(grid, t)) right = t;
+            else low = t + 1;
+        }
+        return low;
     }
 };
